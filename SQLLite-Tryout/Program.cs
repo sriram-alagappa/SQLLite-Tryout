05313ac16bbea21408e8b7c5f3e322146f6b4d53
sriram <salagappa@provocraft.com>
2016-02-20 11:54:40 -0700

    init commit

---
+++ SQLLite-Tryout/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLLite_Tryout
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+
+
+            new SQLLiteTest();
+
+
+
+        }
+    }
+    
+    class SQLLiteTest
+    {
+      
+
+        System.Data.SQLite.SQLiteConnection m_dbConnection;
+        public SQLLiteTest()
+        {
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            string sql = "create table employees (name varchar(20) )";
+
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+        }
+    }
+}
